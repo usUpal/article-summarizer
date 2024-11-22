2deca21524e4990df0ced54cfa3c93e216e2cd4c
usupal <udyan.upal@gmail.com>
2024-11-22 23:43:10 +0600

    Remove unused background script and styles for the Page Summarizer extension

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.action.onClicked.addListener((tab) => {
-  console.log("Clicked on the extension icon");
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ["content.js"],
-  });
-});
